38b489577afbc17d2a4bef2cf2c1117ec2b94f5b
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2020-08-03 23:10:27 +0200

    Fixed player movement

--- src/scripts/objects/fpsText.ts
+++ src/scripts/objects/fpsText.ts
@@ -1,11 +1,13 @@
 export default class FpsText extends Phaser.GameObjects.Text {
   constructor(scene: Phaser.Scene) {
-    super(scene, 10, 10, '', { color: 'black', fontSize: '28px' })
-    scene.add.existing(this)
-    this.setOrigin(0)
+    super(scene, 10, 10, '', {color: 'black', fontSize: '28px'});
+    scene.add.existing(this);
+    this.setOrigin(0);
+    // this.setResolution(800);
+    this.setScale(0.5);
   }
 
   public update() {
-    this.setText(`fps: ${Math.floor(this.scene.game.loop.actualFps)}`)
+    this.setText(`fps: ${Math.floor(this.scene.game.loop.actualFps)}`);
   }
 }
--- src/scripts/objects/player.ts
+++ src/scripts/objects/player.ts
@@ -1,14 +1,18 @@
 import Gun from './gun';
 
 export default class Player extends Phaser.GameObjects.Sprite {
-  private atTop: boolean = false;
-  private atBottom: boolean = false;
-  private gunTopRadius: number = 60;
-  private gunBottomRadius: number = 20;
+  // Parameters
+  private pointX = 0;
+  private pointY = 0;
   private flip: boolean = false;
   private speed: number = 100;
   private jumpSpeed: number = 300;
+
+  // GameObjects
+  pointer: Phaser.GameObjects.Rectangle;
   gun: Gun;
+
+  // Inputs
   left: Phaser.Input.Keyboard.Key;
   right: Phaser.Input.Keyboard.Key;
   up: Phaser.Input.Keyboard.Key;
@@ -21,6 +25,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
   }
 
   create() {
+    const {x, y} = this.getCenter();
     // Player setting
     this.scene.add.existing(this);
     this.scene.physics.world.enable(this);
@@ -34,8 +39,12 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.inputs();
 
     // Gun setting
-    const {x, y} = this.getCenter();
     this.gun = new Gun(this.scene, x, y);
+
+    this.pointX = x;
+    this.pointY = y;
+    this.pointer = this.scene.add.rectangle(x, y, 10, 10, 100);
+    this.pointer.setVisible(false);
   }
 
   // UPDATE -------------
@@ -45,89 +54,30 @@ export default class Player extends Phaser.GameObjects.Sprite {
   }
 
   private gunControl() {
-    let vec1 = new Phaser.Math.Vector2(2, 2).normalize();
-    let vec2 = new Phaser.Math.Vector2(1, 1).setToPolar(10, 40);
-
-    const {angle, rotation} = this.gun;
-    const {x, y} = this.getCenter();
-
-    let reverse = Phaser.Math.Angle.Reverse(angle);
-    let rad2Deg = Phaser.Math.RadToDeg(angle);
-    let deg2Rad = Phaser.Math.DegToRad(rotation);
-
-    console.log({angle, rotation}, {reverse, rad2Deg, deg2Rad});
+    let {x, y} = this.getCenter();
+    let target = Phaser.Math.Angle.Between(x, y, this.pointX, this.pointY);
 
     this.gun.setPosition(x, y);
-    console.log(Math.cos(100), Math.sin(100));
+    this.gun.setOrigin(0.5, 0.5);
+    this.gun.setRotation(target);
 
-    if (this.flip) {
-      this.gun.setOrigin(1, 0.5);
-      this.gun.flipX = true;
+    if (!this.flip) {
+      if (this.gunUp.isDown) {
+        this.pointY -= 1;
+      } else if (this.gunDown.isDown) {
+        this.pointY += 1;
+      }
+      this.pointX = x + 30;
     } else {
-      this.gun.setOrigin(0, 0.5);
-      this.gun.flipX = false;
-    }
-    if (this.gunUp.isDown) {
-      this.gun.body.setAngularVelocity(-40);
-    } else if (this.gunDown.isDown) {
-      this.gun.body.setAngularVelocity(40);
-    } else {
-      this.gun.body.setAngularVelocity(0);
+      if (this.gunUp.isDown) {
+        this.pointY -= 1;
+      } else if (this.gunDown.isDown) {
+        this.pointY += 1;
+      }
+      this.pointX = x - 30;
     }
 
-    // if (!this.flip) {
-    //   if (angle < -60 && angle > -65) {
-    //     this.gun.angle = -60;
-    //   }
-    //   if (angle > 20 && angle < 25) {
-    //     this.gun.angle = 20;
-    //   }
-    //   if (angle >= -60 && angle <= 20) {
-    //     if (this.gunUp.isDown) {
-    //       this.gun.body.setAngularVelocity(-40);
-    //     } else if (this.gunDown.isDown) {
-    //       this.gun.body.setAngularVelocity(40);
-    //     } else {
-    //       this.gun.body.setAngularVelocity(0);
-    //     }
-    //   } else if (angle >= -120 && angle >= 160) {
-    //     this.gun.angle = angle + 180;
-    //   } else {
-    //     this.gun.body.setAngularVelocity(0);
-    //   }
-    // } else {
-    //   if (angle > -120 && angle < -115) {
-    //     console.log(0);
-
-    //     this.gun.angle = -120;
-    //   }
-    //   if (angle < 160 && angle > 155) {
-    //     console.log(1);
-
-    //     this.gun.angle = 160;
-    //   }
-    //   if (angle <= -120 && angle >= 160) {
-    //     if (this.gunUp.isDown) {
-    //       this.gun.body.setAngularVelocity(40);
-    //     } else if (this.gunDown.isDown) {
-    //       this.gun.body.setAngularVelocity(-40);
-    //     } else {
-    //       this.gun.body.setAngularVelocity(0);
-    //     }
-    //   } else if (angle >= -60 && angle <= 20) {
-    //     this.gun.angle = -(angle + 180);
-    //   } else {
-    //     this.gun.body.setAngularVelocity(0);
-    //   }
-    // }
-
-    // if (this.gunUp.isDown) {
-    //   this.gun.body.setAngularVelocity(-40);
-    // } else if (this.gunDown.isDown) {
-    //   this.gun.body.setAngularVelocity(40);
-    // } else {
-    //   this.gun.body.setAngularVelocity(0);
-    // }
+    this.pointer.setPosition(this.pointX, this.pointY);
   }
 
   private playerControl() {
